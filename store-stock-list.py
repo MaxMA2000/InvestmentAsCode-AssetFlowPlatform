6779fbc0bf14686443f3385961260f2ce32ea48f
MaxMA2000 <maxmahklife@gmail.com>
2024-03-01 20:20:24 +0800

    refactor(IAC-12): refactor the adhoc stock list job

--- store-stock-list.py
+++ store-stock-list.py
@@ -1,45 +1,49 @@
-import requests
 import os
-
+import requests
 from dotenv import load_dotenv
-
-# Load the environment variables from the .env file
-load_dotenv()
-
-# Access the API key value from the environment
-api_key = os.getenv('FMP_API_KEY')
-
-api_url = f"https://financialmodelingprep.com/api/v3/stock/list?apikey={api_key}"
-
-# Send GET request
-response = requests.get(api_url)
-
-# Check if the request was successful (status code 200)
-if response.status_code == 200:
-    # Convert the response to a list of dictionaries
-    data = response.json()
-    # print(data)
-else:
-    print("Error occurred:", response.status_code)
-
-
-
 from pymongo import MongoClient
 
-# Create a MongoClient object and specify the connection URL
-client = MongoClient('mongodb://localhost:27017/')
+load_dotenv()
 
+api_key = os.getenv('FMP_API_KEY')
+mongo_server_url = os.getenv('MONGO_SERVER_URL')
+
+api_url = f"https://financialmodelingprep.com/api/v3/stock/list?apikey={api_key}"
 
 database_name = "ingestion_database"
 collection_name = "stock_list"
 
-# Access a specific database
-db = client[database_name]
 
-# Access a specific collection within the database
-collection = db[collection_name]
+def send_get_request(api_url):
+  try:
+    # Send GET request to API
+    response = requests.get(api_url)
+
+    if response.status_code == 200:
+       return f"Error: Unexpected response {response}"
+
+    data = response.json()
+
+    return data
+
+  except requests.exceptions.HTTPError as error:
+      return f"Error: Unexpected response {error}"
 
 
-# Insert each item in the "data" list into the collection
-for item in data:
-    collection.insert_one(item)
+data = send_get_request()
+
+
+if type(data) == list:
+
+    # Create Mongo Client and connect with MongoDB Server
+    client = MongoClient(mongo_server_url)
+
+    db = client[database_name]
+    collection = db[collection_name]
+
+    # Insert each item into the collection
+    for item in data:
+        collection.insert_one(item)
+
+
+
