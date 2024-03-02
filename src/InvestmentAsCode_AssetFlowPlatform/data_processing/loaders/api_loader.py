bc1417fd4d2d4bd9ae66315e3d9c7757acec52b5
MaxMA2000 <maxmahklife@gmail.com>
2024-03-02 23:45:18 +0800

    feat(IAC-12): add the type hint for api loader class

--- src/InvestmentAsCode_AssetFlowPlatform/data_processing/loaders/api_loader.py
+++ src/InvestmentAsCode_AssetFlowPlatform/data_processing/loaders/api_loader.py
@@ -2,36 +2,40 @@ import os
 import requests
 from loader import Loader
 from dotenv import load_dotenv
+from typing import Dict, Any
 
 load_dotenv()
 
 class ApiLoader(Loader):
+    api_url: str
+    __api_key: str
+    parameters: Dict[str, Any]
 
-  def __init__(self, config):
-      super().__init__(config)
-      self.api_url = config.get("api_url")
-      self.__api_key = os.getenv('FMP_API_KEY')
-      self.parameters = self._add_api_key(config.get("parameters"))
+    def __init__(self, config: Dict[str, Any]):
+        super().__init__(config)
+        self.api_url = config.get("api_url")
+        self.__api_key = os.getenv('FMP_API_KEY')
+        self.parameters = self._add_api_key(config.get("parameters"))
 
-  def _add_api_key(self, parameters):
-    parameters["api_key"] = self.__api_key
-    return parameters
+    def _add_api_key(self, parameters: Dict[str, Any]) -> Dict[str, Any]:
+        parameters["api_key"] = self.__api_key
+        return parameters
 
-  def fetch_data(self):
-    try:
-      print(f"Sending GET API Requests to {self.api_url}")
-      response = requests.get(self.api_url, self.parameters)
+    def fetch_data(self) -> Any:
+        try:
+            print(f"Sending GET API Requests to {self.api_url}")
+            response = requests.get(self.api_url, self.parameters)
 
-      print(f"Finish GET requests, Reponse Status Code = {response.status_code}")
-      if response.status_code != 200:
-        return f"Error: Unexpected response {response}"
+            print(f"Finish GET requests, Response Status Code = {response.status_code}")
+            if response.status_code != 200:
+                return f"Error: Unexpected response {response}"
 
-      data = response.json()
+            data = response.json()
 
-      return data
+            return data
 
-    except requests.exceptions.HTTPError as error:
-        return f"Error: Unexpected response {error}"
+        except requests.exceptions.HTTPError as error:
+            return f"Error: Unexpected response {error}"
 
-  def load_data(self, data):
-      pass
+    def load_data(self, data: Any):
+        pass
