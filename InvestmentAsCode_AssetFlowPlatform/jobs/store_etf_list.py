28eeb710ed8cd3183934f753778a56178c63b334
MaxMA2000 <maxmahklife@gmail.com>
2024-03-03 14:23:29 +0800

    feat(IAC-12): refactor the store_etf_list program

---
+++ InvestmentAsCode_AssetFlowPlatform/jobs/store_etf_list.py
@@ -0,0 +1,23 @@
+from InvestmentAsCode_AssetFlowPlatform.data_processing.loaders.api_loader import ApiLoader
+from InvestmentAsCode_AssetFlowPlatform.data_processing.savers.mongo_saver import MongoSaver
+
+# Load the data from API
+loader_config = {
+  "api_url": "https://financialmodelingprep.com/api/v3/stock/list",
+  "api_key_name": "FMP_API_KEY",
+  "parameters": {}
+}
+
+api_loader = ApiLoader(loader_config)
+data = api_loader.fetch_data()
+
+# Save the data into MongoDB
+saver_config= {
+    "database_name": "ingestion_database",
+    "collection_name": "exchange_traded_fund_list"
+}
+
+saver = MongoSaver(saver_config)
+saver.save_data(data)
+
+
