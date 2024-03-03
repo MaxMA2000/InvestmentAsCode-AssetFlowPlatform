08a4a41b7b2c57c3ca08c5044ec062edb17f7ff1
MaxMA2000 <maxmahklife@gmail.com>
2024-03-03 14:23:12 +0800

    feat(IAC-12): update the type hint in mongo_saver

--- InvestmentAsCode_AssetFlowPlatform/data_processing/savers/mongo_saver.py
+++ InvestmentAsCode_AssetFlowPlatform/data_processing/savers/mongo_saver.py
@@ -2,22 +2,22 @@ import os
 from pymongo import MongoClient
 from .saver import Saver
 from dotenv import load_dotenv
-from typing import Dict, Any
+from typing import Dict, Any, List
 
 class MongoSaver(Saver):
     """Child class to save data to MongoDB"""
 
-    def __init__(self, config):
+    def __init__(self, config: Dict[str, str]):
         super().__init__(config)
         self.database_name = config.get('database_name')
         self.collection_name = config.get('collection_name')
         self.mongo_server_url = self._add_mongo_server_url()
 
     @staticmethod
-    def _add_mongo_server_url():
+    def _add_mongo_server_url() -> str:
         return os.getenv('MONGO_SERVER_URL')
 
-    def save_data(self, data):
+    def save_data(self, data: List[Dict[str, Any]]):
         # Create a MongoDB client and connect to the MongoDB server
         client = MongoClient(self.mongo_server_url)
 
