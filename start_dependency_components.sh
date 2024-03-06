a589f96fc684945f1fe9dc5f3a84994cf9e6bd9a
MaxMA2000 <maxmahklife@gmail.com>
2024-03-06 22:53:45 +0800

    setup(IAC-12): update the dependency startup sh

---
+++ start_dependency_components.sh
@@ -0,0 +1,7 @@
+# start mongodb locally
+# brew services start mongodb-community@7.0
+sudo mongod --dbpath=/Users/maxma/data/db
+
+# install & start airflow in local repo
+# https://coder2j.com/airflow-tutorial/install-airflow-on-mac/
+airflow standalone
--- start_mongodb.sh
+++
@@ -1,3 +0,0 @@
-# brew services start mongodb-community@7.0
-
-sudo mongod --dbpath=/Users/maxma/data/db
