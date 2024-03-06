e27fc3217bc2ad581af924f551ff3413b438f6bd
MaxMA2000 <maxmahklife@gmail.com>
2024-03-06 23:19:47 +0800

    feat(IAC-12): create the invest FMPData into mongodb DAG

---
+++ airflow/dags/Ingest_FMPData_To_Mongodb.py
@@ -0,0 +1,37 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime
+
+from InvestmentAsCode_AssetFlowPlatform.jobs import store_company_general_info
+from InvestmentAsCode_AssetFlowPlatform.jobs import store_etf_list
+from InvestmentAsCode_AssetFlowPlatform.jobs import store_stock_list
+
+schedule_interval = "0 18 * * *"
+
+default_args = {
+    "owner": "airflow",
+    "start_date": datetime(2024, 3, 5),
+}
+
+with DAG(
+    "Ingest_FMPData_To_Mongodb",
+    default_args=default_args,
+    schedule_interval=schedule_interval,
+) as dag:
+
+    task_1 = PythonOperator(
+        task_id="store_company_general_info",
+        python_callable=store_company_general_info.task,
+    )
+
+    task_2 = PythonOperator(
+        task_id="store_etf_list",
+        python_callable=store_etf_list.task,
+    )
+
+    task_3 = PythonOperator(
+        task_id="store_stock_list",
+        python_callable=store_stock_list.task,
+    )
+
+    task_1 >> task_2 >> task_3
