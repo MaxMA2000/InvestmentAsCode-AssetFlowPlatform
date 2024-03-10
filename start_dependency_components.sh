53d8769c850063ab1948d2fbba9a029b2f628148
MaxMA2000 <maxmahklife@gmail.com>
2024-03-10 14:35:52 +0800

    update(IAC-8): update the start dependency component sh

--- start_dependency_components.sh
+++ start_dependency_components.sh
@@ -5,3 +5,9 @@ sudo mongod --dbpath=/Users/maxma/data/db
 # install & start airflow in local repo
 # https://coder2j.com/airflow-tutorial/install-airflow-on-mac/
 airflow standalone
+
+
+# check port usage
+sudo lsof -i :8080
+# kill port application
+sudo kill -9 99213
