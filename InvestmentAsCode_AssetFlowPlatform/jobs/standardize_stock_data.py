9e8fd8b7015cf8ca0e52a6f8e38a86b025470640
MaxMA2000 <maxmahklife@gmail.com>
2024-03-11 22:15:21 +0800

    feat(IAC-8): add the action items in standard_stock_data job

---
+++ InvestmentAsCode_AssetFlowPlatform/jobs/standardize_stock_data.py
@@ -0,0 +1,9 @@
+
+
+
+# step 1: load stock_prices to get list of stocks (from mongo)
+# step 2: store the list of stocks, for each stock inside, run the logic following:
+# step 3: based on this stock symbol, get stock info  (from mongo)
+# step 4: insert / update stock info into postgre asset table
+# step 5: get the inserted asset id, combined with the stock prices
+# step 6: format the stock daily price data, insert / update stock table
